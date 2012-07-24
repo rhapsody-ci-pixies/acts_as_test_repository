511b839072fd8b101c095001ba0c44532f4a0cda
Evan Stoner <evan.stoner@gmail.com>
2012-07-23 17:27:31 -0700

    Created intentionally failing test to fail the test for tests failing.

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -5,4 +5,7 @@ describe Test do
   it "must provide something" do
     Test.something.must_equal "something"
   end
+  it "must provide failure" do
+    Test.something.must_equal "failure"
+  end
 end
