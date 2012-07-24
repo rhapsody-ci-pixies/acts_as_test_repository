446aa6a3d6ec3ce1199ca3c5bf0f8d48f5c07692
Evan Stoner <evan.stoner@gmail.com>
2012-07-23 17:28:51 -0700

    Revert "Created intentionally failing test to fail the test for tests failing."
    This reverts commit 511b839072fd8b101c095001ba0c44532f4a0cda.

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -5,7 +5,4 @@ describe Test do
   it "must provide something" do
     Test.something.must_equal "something"
   end
-  it "must provide failure" do
-    Test.something.must_equal "failure"
-  end
 end
